623fa210241454c09cdacab49c320d030b1b53a7
MaryG <MaryG>
2021-03-17 20:49:01 +0300

    procfile added

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.post('/failure.html', (req, res) => {
     res.redirect('/');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT  || 3000, () => {
     console.log('Server started on 3000 port')
 });
 
