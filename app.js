a75ff1f1e0c445d79f6abe36c59252a069f3e507
MaryG <MaryG>
2021-03-17 10:50:01 +0300

    refactoring

--- app.js
+++ app.js
@@ -2,66 +2,52 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
 const https = require('https');
 
 const app = express();
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/signup.html`);
 });
 
 app.post('/', (req, res) => {
-    const firstName = req.body.fName;
-    const lastName = req.body.lName;
-    const email = req.body.email;
-    console.log(`${firstName} ${lastName} ${email}`);
-
-    const userData = {
-        members: [{
-            email_address: email,
-            status: 'subscribed',
-            merge_fields: {
-                FName: firstName,
-                LName: lastName
-            }
-        }]
-    };
-
-    const jsonUserData = JSON.stringify(userData);
-
-    const mailchimpUrl = 'https://us1.api.mailchimp.com/3.0/lists/39103f4035';
-    const options = {
-        method: "POST",
-        auth: "mary:6e9a36dafdd8258b85dc22f541e10b0a-us1"
-    };
-
-    const mailchimpRequest = https.request(mailchimpUrl, options, (mailchimpResponse) => {
-        mailchimpResponse.on("data", (data) => {
-            const responseBody = JSON.parse(data);
-            const errors = responseBody.errors;
-
-            console.log(mailchimpResponse.statusCode);
-            mailchimpResponse.statusCode === 200 ? res.sendFile(`${__dirname}/success.html`) : res.sendFile(`${__dirname}/failure.html`);
-        })
-    });
-    mailchimpRequest.write(jsonUserData);
-    mailchimpRequest.end();
+    sendUserDataToMailchimp(res, getUserData(req));
 });
 
 app.post('/failure.html', (req, res) => {
     res.redirect('/');
 });
 
-
 app.listen(3000, () => {
     console.log('Server started on 3000 port')
-
 });
 
-//6e9a36dafdd8258b85dc22f541e10b0a-us1
-//39103f4035
+function sendUserDataToMailchimp(res, userData) {
+    const mailchimpUrl = 'https://us1.api.mailchimp.com/3.0/lists/39103f4035';
+    const options = {
+        method: "POST",
+        auth: "mary:6e9a36dafdd8258b85dc22f541e10b0a-us1"
+    };
+    const mailchimpRequest = https.request(mailchimpUrl, options, (mailchimpResponse) => {
+        mailchimpResponse.on("data", (data) => {
+            mailchimpResponse.statusCode === 200 ? res.sendFile(`${__dirname}/success.html`) : res.sendFile(`${__dirname}/failure.html`);
+        });
+    });
+    mailchimpRequest.write(userData);
+    mailchimpRequest.end();
+}
+
+function getUserData(req) {
+    return JSON.stringify({
+        members: [{
+            email_address: req.body.email,
+            status: 'subscribed',
+            merge_fields: {
+                FName: req.body.fName,
+                LName: req.body.lName
+            }
+        }]
+    });
+}
